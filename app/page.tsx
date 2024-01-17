b5786885e918afac6c815dd490bbc748d271bcb4
BirdBird <birdbirdmurmur@gmail.com>
2024-01-17 20:14:36 +0800

    create 5 components & add Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Camp from "@/components/Camp";
+import Feature from "@/components/Feature";
+import GetAPP from "@/components/GetAPP";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
-    <h1 className="font -bold text-teal-500 text-2xl">
-      Hello world
-    </h1>
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Feature />
+      <GetAPP />
+    </>
   );
 }
---
+++ components/GetAPP.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GetAPP = () => {
+    return (
+        <div>GetAPP</div>
+    )
+}
+
+export default GetAPP
